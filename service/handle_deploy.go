006271199d9d18626979456f328e18f5a963b9e6
Mike Nomitch <mnomitch@hashicorp.com>
2021-03-16 23:31:31 -0700

    POC basically done

--- service/handle_deploy.go
+++ service/handle_deploy.go
@@ -1,16 +1,53 @@
 package service
 
 import (
-	"io"
+	"encoding/json"
+	"fmt"
 	"net/http"
+	"os"
+	"os/exec"
 )
+
+type DeployRequest struct {
+	Name      string            `json:"name"`
+	Variables map[string]string `json:"variables"`
+}
 
 func HandleDeploy(w http.ResponseWriter, req *http.Request) {
 	header := w.Header()
-	header.Set("Content-Type", "application/json")
 	header.Set("Access-Control-Allow-Origin", "*")
-	header.Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+	header.Set("Access-Control-Allow-Methods", "POST, OPTIONS")
 	header.Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
 
-	io.WriteString(w, "Hello, world!\n")
+	if (*req).Method == "OPTIONS" {
+		return
+	}
+
+	// data, err := ioutil.ReadAll(req.Body)
+	// asString := string(data)
+
+	// fmt.Println(asString)
+
+	decoder := json.NewDecoder(req.Body)
+
+	var dr DeployRequest
+
+	err := decoder.Decode(&dr)
+
+	if err != nil {
+		panic(err)
+	}
+
+	variableArgs := []string{"install", dr.Name}
+
+	for k, v := range dr.Variables {
+		flag := fmt.Sprintf("-%s=%s", k, v)
+		variableArgs = append(variableArgs, flag)
+	}
+
+	cmd := exec.Command("bindle", variableArgs...)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	_ = cmd.Run()
+
 }
